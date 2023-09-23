8e269072491d6bcd8dff9a8ef5bb37c0c8c421ac
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-09-23 12:30:49 +0100

    Added Roles.initPermissions and Roles.initialiseListeners to the main class, to allow initialisation of the Listener and of permissions within the Permission Manager (the factory called is currently still unimplemented)

--- src/main/java/io/curiositycore/roles/Roles.java
+++ src/main/java/io/curiositycore/roles/Roles.java
@@ -1,17 +1,30 @@
 package io.curiositycore.roles;
 
+import io.curiositycore.roles.listeners.UserListener;
+import io.curiositycore.roles.model.factories.PermssionFactory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Roles extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        initPermissions();
+        initialiseListeners();
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void initPermissions(){
+        PermssionFactory permssionFactory = new PermssionFactory();
+        permssionFactory.setConfigSection(this.getConfig().getConfigurationSection("spigot-discord"));
+        permssionFactory.build();
+    }
+
+    private void initialiseListeners(){
+        this.getServer().getPluginManager().registerEvents(new UserListener(),this);
     }
 }
