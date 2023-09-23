ced61ff6c9d9e9e7e7be084ee7a9533f7a8099bd
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-09-23 12:35:41 +0100

    Addition of NotImplementedExceptions to:
    - PermissionFactory.build
    - Role.getId
    - Role.getLinkedPermissions
    - Role.getPriority
    
    Refactored name of PermssionFactory.java to PermissionFactory.java

--- src/main/java/io/curiositycore/roles/model/factories/PermissionFactory.java
+++ src/main/java/io/curiositycore/roles/model/factories/PermissionFactory.java
@@ -3,7 +3,7 @@ package io.curiositycore.roles.model.factories;
 import org.apache.commons.lang3.NotImplementedException;
 import org.bukkit.configuration.ConfigurationSection;
 
-public class PermssionFactory implements Factory{
+public class PermissionFactory implements Factory{
     private ConfigurationSection permissionSection;
 
     @Override
@@ -13,6 +13,7 @@ public class PermssionFactory implements Factory{
 
     @Override
     public void build() {
+        //TODO implement the build (which will involve the adding of all the permissions to the PermissionManager)
         throw new NotImplementedException("Not implemented permission build yet");
     }
 }
--- src/main/java/io/curiositycore/roles/model/permissions/Role.java
+++ src/main/java/io/curiositycore/roles/model/permissions/Role.java
@@ -1,18 +1,23 @@
 package io.curiositycore.roles.model.permissions;
 
+import org.apache.commons.lang3.NotImplementedException;
+
 public class Role implements Permission<Group>{
     @Override
     public String getId() {
-        return null;
+        //TODO implement this
+        throw new NotImplementedException("Getting the discord role's id is not yet implemented");
     }
 
     @Override
     public int getPriority() {
-        return 0;
+        //TODO implement this
+        throw new NotImplementedException("Priority selection for discord roles not yet implemented");
     }
 
     @Override
     public Group[] getLinkedPermissions() {
-        return new Group[0];
+        //TODO implement this
+        throw new NotImplementedException("Getting all the Groups linked to this discord role is not yet implemented");
     }
 }
